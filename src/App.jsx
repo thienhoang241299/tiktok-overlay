70d3d7c612d8aea99c355e277234ebfc155a7dfb
Vit Lang Thang <thienhoangba99@nhoangba99gmail.com>
2025-05-21 09:39:05 +0700

    updaet code

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/m" />} />
         <Route path="/m" element={<ManageSongs />} />
         <Route path="/o" element={<OverlaySongList />} />
       </Routes>
