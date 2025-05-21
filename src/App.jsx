64869bb9c2991024956b87c89acefb6a1ce039aa
Vit Lang Thang <thienhoangba99@nhoangba99gmail.com>
2025-05-21 09:41:22 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/m" />} />
+        <Route path="/" element={<ManageSongs />} />
         <Route path="/m" element={<ManageSongs />} />
         <Route path="/o" element={<OverlaySongList />} />
       </Routes>
